960f9f5e4dabbd2cc7082c6af3e10fa79e0d9233
xiongchiamiov <xiong.chiamiov@gmail.com>
2013-05-06 21:44:03 -0700

    Package ghgrep.
    Packaging is such a pain.  Honestly, it's a lot easier to do this in Ruby - I
    just dump in a couple files and go.  Although, admittedly, some of my
    difficulties here come from a stubborn refusal to use reStructuredText.

--- README.md
+++ README.md
@@ -19,3 +19,20 @@ List all repositories that don't have a license file:
 
     ghgrep -v (COPYING|LICENSE)
 
+# Installation
+
+    [$]> pip install ghgrep
+
+or
+
+    [$]> easy_install ghgrep
+
+# Hacking
+
+I highly recommend using virtualenv:
+
+    [$]> virtualenv --no-site-packages --distribute env
+    [$]> source env/bin/activate
+    [$]> pip install -r requirements.txt
+    [$]> pip install -e . # So we can import the version from inside bin/ .
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+from distutils.core import setup
+
+from ghgrep import VERSION
+
+# I really prefer Markdown to reStructuredText.  PyPi does not.  This allows me
+# to have things how I'd like, but not throw complaints when people are trying
+# to install the package and they don't have pypandoc or the README in the
+# right place.
+try:
+   import pypandoc
+   description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   description = ''
+
+setup(
+   name = 'ghgrep',
+   version = VERSION,
+   author = 'James Pearson',
+   author_email = 'pearson@changedmy.name',
+   packages = ['ghgrep'],
+   scripts = ['bin/ghgrep'],
+   url = 'https://github.com/xiongchiamiov/ghgrep',
+   license = 'WTFPL',
+   description = 'List all your Github repositories containing a filename matching a search pattern.',
+   long_description = description,
+   install_requires = [
+      'docopt >= 0.6.1',
+      'envoy >= 0.0.2',
+      'github3.py >= 0.6.1, < 0.7',
+   ],
+)
+
